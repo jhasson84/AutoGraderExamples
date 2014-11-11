424228a0d11f77935555a2163097145b88e04db9
jhasson84 <jhasson@uncc.edu>
2014-11-11 17:04:14 -0500

    unrefactored branch check

--- git-immersion/autograder/mvp_spec.rb
+++ git-immersion/autograder/mvp_spec.rb
@@ -1,41 +1,39 @@
 require 'octokit'
 require 'rspec'
 
+
 describe "Github" do
   it "should find a gitimmersion repository for the #{ENV['GITHUB_USERNAME']+'/gitimmersion'} [5 points]" do
-    (Octokit.repository? ENV['GITHUB_USERNAME']+'/gitimmersion').should be_true
+   (Octokit.repository? ENV['GITHUB_USERNAME']+'/gitimmersion').should be_true
   end
 
    it "should have at least 3 recent commits in name of #{ENV['GITHUB_USERNAME']+'/gitimmersion'}  [10 points]" do
-      repo_name = ENV['GITHUB_USERNAME']+'/gitimmersion'
-      commits = Octokit.commits_since(repo_name,'2014-01-01')
-      commits.count.should be > 3
-      commits.each {|c| c.author.login.should eq ENV['GITHUB_USERNAME']}
+    repo_name = ENV['GITHUB_USERNAME']+'/gitimmersion'
+    commits = Octokit.commits_since(repo_name,'2014-01-01')
+    commits.count.should be > 3
+    commits.each {|c| c.author.login.should eq ENV['GITHUB_USERNAME']}
    end
 
-   it "should have at least 6 recent commits in name of #{ENV['GITHUB_USERNAME']+'/gitimmersion'}  [15 points]" do
-      repo_name = ENV['GITHUB_USERNAME']+'/gitimmersion'
-      commits = Octokit.commits_since(repo_name,'2014-01-01')
-      commits.count.should be > 3
-      commits.each {|c| c.author.login.should eq ENV['GITHUB_USERNAME']}
+   it "should have at least 6 recent commits in name of #{ENV['GITHUB_USERNAME']+'/gitimmersion'}  [10 points]" do
+     repo_name = ENV['GITHUB_USERNAME']+'/gitimmersion'
+     commits = Octokit.commits_since(repo_name,'2014-01-01')
+     commits.count.should be > 6
+     commits.each {|c| c.author.login.should eq ENV['GITHUB_USERNAME']}
    end
 
-   it "should have at least 9 recent commits in name of #{ENV['GITHUB_USERNAME']+'/gitimmersion'}  [15 points]" do
-      repo_name = ENV['GITHUB_USERNAME']+'/gitimmersion'
-      commits = Octokit.commits_since(repo_name,'2014-01-01')
-      commits.count.should be > 3
-      commits.each {|c| c.author.login.should eq ENV['GITHUB_USERNAME']}
+   it "should have at least 9 recent commits in name of #{ENV['GITHUB_USERNAME']+'/gitimmersion'}  [10 points]" do
+     repo_name = ENV['GITHUB_USERNAME']+'/gitimmersion'
+     commits = Octokit.commits_since(repo_name,'2014-01-01')
+     commits.count.should be > 9
+     commits.each {|c| c.author.login.should eq ENV['GITHUB_USERNAME']}
    end
 
-  #it 'should not have any forks  [10 points]' do
-      #repo_name = ENV['GITHUB_USERNAME']+'/gitimmersion'      
-      #forks = Octokit.forks(repo_name)      
-      #forks[0].fork.should be 'false' 
-      #forks.each{|c| c.count.should be = 'nil'}    
-  #end  
+ it "should have a more than 1 branch #{ENV['GITHUB_USERNAME']+'/gitimmersion'} [15 points]" do
+   Octokit.branches(ENV['GITHUB_USERNAME']+'/gitimmersion').count.should be > 1
+ end 
 
   #it 'should have at least merge commit [5 points]' do
-    #repo_name = ENV['GITHUB_USERNAME']+'/gitimmersion'
+   #repo_name = ENV['GITHUB_USERNAME']+'/gitimmersion'
     #commits = Octokit.commits_since(repo_name,'2014-01-01')
     #collection = commits.collect{|c| c.commit.message.include? 'merge'}
     #collection.should include(true)
